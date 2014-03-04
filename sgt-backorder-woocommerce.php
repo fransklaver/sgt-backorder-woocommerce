f59914266741daeb08091a4df5049749f2529dbf
Frans Klaver <fransklaver@gmail.com>
2014-03-04 23:14:52 +0100

    Add page that lists all items with negative stock

--- sgt-backorder-woocommerce.php
+++ sgt-backorder-woocommerce.php
@@ -32,8 +32,16 @@ load_plugin_textdomain('sgt-backorder-woocommerce', false, basename(dirname(__FI
 
 if (is_admin())
 {
+	add_action('admin_menu', 'add_generate_back_order_menu');
+
 	add_action( 'woocommerce_product_options_general_product_data', 'sgt_add_custom_general_fields' );
 	add_action( 'woocommerce_process_product_meta', 'sgt_add_custom_general_fields_save' );
+}
+
+function add_generate_back_order_menu()
+{
+	add_menu_page(__('Generate back order', 'sgt-backorder-woocommerce'), __('Generate back order', 'sgt-backorder-woocommerce'), 'export', 'sgt_back_order', 'add_back_order_page', null, '60.5');
+
 }
 
 function sgt_add_custom_general_fields()
@@ -61,5 +69,32 @@ function sgt_add_custom_general_fields_save($post_id)
 	$bulk_amount = $_POST['_sgt_bulk_amount'];
 	if (!empty($bulk_amount))
 		update_post_meta($post_id, '_sgt_bulk_amount', esc_attr($bulk_amount));
+}
+
+function add_back_order_page()
+{
+	$args = array(
+		'post_type' => 'product'
+	);
+	$loop = new WP_Query($args);
+	$product_count = $loop->post_count;
+	if ($loop->have_posts()) {
+		while ($loop->have_posts()) {
+			$post = $loop->next_post();
+			$title = get_the_title($post->ID);
+			$bulk_amount = get_post_meta($post->ID, '_sgt_bulk_amount', true);
+			$stock = get_post_meta($post->ID, '_stock', true);
+
+			echo '<p ';
+			if (empty($bulk_amount) || $bulk_amount == '0' || $stock > 0) {
+				echo 'style="color:red"';
+				$back_order = 0;
+			} else {
+				$back_order = ceil(-$stock / $bulk_amount);
+			}
+
+			echo '>'.$title.' back order = '.$back_order.' ('.$stock.'/'.$bulk_amount.')</p>';
+		}
+	}
 }
 ?>
