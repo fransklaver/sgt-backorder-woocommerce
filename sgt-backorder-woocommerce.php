a04f88e7ed50317bbe2c80fef75128fdb6db7f22
Frans Klaver <fransklaver@gmail.com>
2014-12-30 13:48:35 +0100

    custom fields: add divisor fields
    At some point we need to split the difference between the ordered amount
    and the amount we can order between the customers. For this to work, we
    need to be able to know the minimum amount of items people need to buy
    (sale amount), and the amount of stuff we can add or subtract from an
    order. Obviously we need to start doing smart stuff when storing these
    orders later on.

--- sgt-backorder-woocommerce.php
+++ sgt-backorder-woocommerce.php
@@ -65,6 +65,16 @@ function sgt_add_custom_general_fields()
 	if (empty($bulk_amount))
 		$bulk_amount = '0';
 
+	woocommerce_wp_select(
+		array(
+			'id'          => '_sgt_product_unit',
+			'label'       => __('Unit', 'sgt-backorder-woocommerce'),
+			'desc_tip'    => true,
+			'description' => __('Select the unit for this item', 'sgt-backorder-woocommerce'),
+			'options'     => array('pieces', 'grams')
+		)
+	);
+
 	woocommerce_wp_text_input(
 		array(
 			'id'          => '_sgt_bulk_amount',
@@ -76,6 +86,38 @@ function sgt_add_custom_general_fields()
 			'custom_attributes' => array('step' => 'any', 'min' => '0')
 		)
 	);
+
+	$sale_amount = get_post_meta($post->ID, '_sgt_sale_amount', true);
+	if (empty($sale_amount))
+		$sale_amount = '1';
+
+	woocommerce_wp_text_input(
+		array(
+			'id'          => '_sgt_sale_amount',
+			'label'       => __('Sale amount', 'sgt-backorder-woocommerce'),
+			'placeholder' => $sale_amount,
+			'desc_tip'    => 'true',
+			'description' => __('Fill in how many items are sold in one go', 'sgt-backorder-woocommerce'),
+			'type'        => 'number',
+			'custom_attributes' => array('step' => 'any', 'min' => '0')
+		)
+	);
+
+	$division_amount = get_post_meta($post->ID, '_sgt_division_amount', true);
+	if (empty($division_amount))
+		$division_amount = '1';
+
+	woocommerce_wp_text_input(
+		array(
+			'id'          => '_sgt_division_amount',
+			'label'       => __('Division amount', 'sgt-backorder-woocommerce'),
+			'placeholder' => $division_amount,
+			'desc_tip'    => 'true',
+			'description' => __('Fill in how many items are used to split the difference', 'sgt-backorder-woocommerce'),
+			'type'        => 'number',
+			'custom_attributes' => array('step' => 'any', 'min' => '0')
+		)
+	);
 }
 
 function sgt_add_custom_general_fields_save($post_id)
@@ -83,5 +125,14 @@ function sgt_add_custom_general_fields_save($post_id)
 	$bulk_amount = $_POST['_sgt_bulk_amount'];
 	if (!empty($bulk_amount))
 		update_post_meta($post_id, '_sgt_bulk_amount', esc_attr($bulk_amount));
+	$sale_amount = $_POST['_sgt_sale_amount'];
+	if (!empty($sale_amount))
+		update_post_meta($post_id, '_sgt_sale_amount', esc_attr($sale_amount));
+	$division_amount = $_POST['_sgt_division_amount'];
+	if (!empty($division_amount))
+		update_post_meta($post_id, '_sgt_division_amount', esc_attr($division_amount));
+	$product_unit = $_POST['_sgt_product_unit'];
+	if (!empty($product_unit))
+		update_post_meta($post_id, '_sgt_product_unit', esc_attr($product_unit));
 }
 ?>
