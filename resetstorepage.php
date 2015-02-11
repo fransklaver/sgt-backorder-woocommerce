d3e8e521f6922d1fff6c3c6b665f3e31b8ec6e60
Frans Klaver <fransklaver@gmail.com>
2015-02-11 20:46:42 +0100

    resetstorepage: rewrite product loop
    We can drop the whole wordpress loop thing, so let's do it.

--- resetstorepage.php
+++ resetstorepage.php
@@ -4,15 +4,13 @@ function reset_store()
 	if (!isset($_POST["reset_store"]) || $_POST["reset_store"] != 1)
 		return false;
 
-	$args = array(
-		'post_type' => 'product',
-		'posts_per_page' => -1,
-		'post_status' => 'publish'
-	);
-	$loop = new WP_Query($args);
-	while ($loop->have_posts()) {
-		$post = $loop->next_post();
-		$product = wc_get_product($post->ID);
+	$products = get_posts(array(
+			'post_type' => 'product',
+			'posts_per_page' => -1,
+			'post_status' => 'publish'
+		));
+	foreach ($products as $post) {
+		$product = wc_get_product($post);
 		$product->set_stock(0);
 		wp_update_post(array(
 			'ID' => $post->ID,
